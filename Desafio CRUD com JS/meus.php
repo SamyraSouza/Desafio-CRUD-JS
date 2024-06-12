0e49856e12749c1b2909008d857b68a6c86ab6e4
SamyraSouza <167902992+SamyraSouza@users.noreply.github.com>
2024-06-12 17:07:52 -0300

    arrumar meus livros

--- Desafio CRUD com JS/meus.php
+++ Desafio CRUD com JS/meus.php
@@ -7,9 +7,20 @@ $email = $_POST['pessoa'];
 
 $pdo = new PDO('mysql:host=localhost; dbname=livraria;','root', '1234');
 
-$stmt = $pdo->prepare('select livros.*, autores.Nome, categoria.Nomes, emprestimo.dataDevo from livros join autorlivro on livros.ISBN = autorlivro.ISBN_livro join autores on autores.id=autorlivro.idAutor join categoria on categoria.id=livros.idCategoria join emprestimo on emprestimo.ISBN_liv=livros.ISBN where (SELECT idCliente from emprestimo)= (select id from cliente WHERE email = "'.$email.'")');
+$pegarid = $pdo->prepare('select id from cliente where email = "'.$email.'"');
+
+$pegarid->execute();
+
+$id = $pegarid->fetchAll(PDO::FETCH_ASSOC);
+
+$ids = $id[0];
+
+$i = $ids['id'];
+
+$stmt = $pdo->prepare('select livros.*, autores.Nome, categoria.Nomes, emprestimo.dataDevo from livros join autorlivro on livros.ISBN = autorlivro.ISBN_livro join autores on autores.id=autorlivro.idAutor join categoria on categoria.id=livros.idCategoria join emprestimo on emprestimo.ISBN_liv=livros.ISBN where emprestimo.idCliente='.$i);
 
 $stmt->execute();
+
 
 if($stmt->rowCount() >= 1){
     echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
